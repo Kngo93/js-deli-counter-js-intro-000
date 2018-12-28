0aef8c4919e2a4b8f7e2b91d7608967eff27b831
Kevin Ngo <k.ngo93@outlook.com>
2018-12-28 20:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function nowServing(line){
     return `Currently serving ${line.shift()}.`;
   }
 }
+
+function currentLine(line){
+  var current = [];
+
+  for (var x = 0; x < line.length; x++){
+    current.push(" " + [x+1] + ". " + line[x])
+  }
+
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    return "The line is currently: " + current;
+  }
+
+
+
+}
