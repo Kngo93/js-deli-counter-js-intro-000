acfde6f990c5731ffce3fc147ce9761c824a7bce
Kevin Ngo <k.ngo93@outlook.com>
2018-12-28 20:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(line){
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
-    return "The line is currently: " + current;
+    return "The line is currently:" + current;
   }
 
 
